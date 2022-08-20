cf0f0d7332cde45c4f4f22544dd79f00aaf60a3c
Ivan Pavlyuk <111151964+ivanpavlyuk@users.noreply.github.com>
2022-08-20 10:54:19 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    Error() {
+
+    }
+
+    Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Ошибка активна: " + active);
+        System.out.println("Статус ошибки: " + status);
+        System.out.println("Сообщение об ошибке: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error symbol = new Error(true, 14, "ошибка: не удается найти символ");
+        Error semicolon = new Error(false, 8, "ожидаемая ошибка: ';'");
+        Error outbound = new Error(true, 30, "Array out of bound");
+        error.printInfo();
+        symbol.printInfo();
+        semicolon.printInfo();
+        outbound.printInfo();
+    }
+}
