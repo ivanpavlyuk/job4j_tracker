609de8fa893f322ec8d3f8250488b1adc6c009c5
Ivan Pavlyuk <111151964+ivanpavlyuk@users.noreply.github.com>
2022-08-21 17:22:00 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,28 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book crime = new Book("Crime and punishment", 700);
+        Book war = new Book("War and peace", 1500);
+        Book code = new Book("Clean code", 900);
+        Book pride = new Book("Pride and prejudice", 800);
+        Book[] list = {crime, war, code, pride};
+        for (int index = 0; index < list.length; index++) {
+            Book book = list[index];
+            System.out.println(book.getName() + " - " + book.getPages() + " pages");
+        }
+        Book book = list[0];
+        list[0] = list[3];
+        list[3] = book;
+        for (int index = 0; index < list.length; index++) {
+            book = list[index];
+            System.out.println(book.getName() + " - " + book.getPages() + " pages");
+        }
+        for (int index = 0; index < list.length; index++) {
+            book = list[index];
+            if (book.getName().equals("Clean code")) {
+                System.out.println(book.getName() + " - " + book.getPages() + " pages");
+            }
+        }
+    }
+}
