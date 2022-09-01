a201a1b6c67d68dac6c3e2b0b6bc086a462d2a77
Ivan Pavlyuk <111151964+ivanpavlyuk@users.noreply.github.com>
2022-09-01 22:35:27 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (key.equals(value[index])) {
+                rsl = index;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("No such element found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[]words = {"Ivan", "Petr", "Sidor"};
+        try {
+            indexOf(words, "Bill");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
