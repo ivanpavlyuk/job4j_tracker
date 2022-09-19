fe6a35e234eb95ec717516c756d9deef7be299a1
Ivan Pavlyuk <111151964+ivanpavlyuk@users.noreply.github.com>
2022-09-19 18:41:00 +0300

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        int first = Integer.parseInt((left.split("\\."))[0]);
+        int second = Integer.parseInt((right.split("\\."))[0]);
+        return Integer.compare(first, second);
+    }
+}
